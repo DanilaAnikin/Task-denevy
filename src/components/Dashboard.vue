f8e1d5fcddb87c64edad8982ab398b47c91fcba0
Danila Anikin <danila.s.anikin@gmail.com>
2023-04-02 15:34:21 +0200

    deleted comments

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -56,22 +56,6 @@ const findWeather = () => {
   .catch(err => alert("Wrong city name!"))
   cityInput.value = ''
 }
-// button.addEventListener('click', function(name){
-// fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
-// .then(response => response.json())
-// .then(data => {
-//   var tempValue = data['main']['temp'];
-//   var nameValue = data['name'];
-//   var descValue = data['weather'][0]['description'];
-
-
-
-// })
-
-// .catch(err => alert("Wrong city name!"));
-// })
-
-
 </script>
 
 <template>
